5da08c16767833a075646a0e81448289c644a30a
Bitangsha Ray <bitangsha2014@gmail.com>
2025-06-09 06:54:29 +0530

    Initial commit

---
+++ src/main/java/com/foody/foodyws/FoodywsApplication.java
@@ -0,0 +1,13 @@
+package com.foody.foodyws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodywsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodywsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foody/foodyws/FoodywsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foody.foodyws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodywsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
